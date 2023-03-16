696e22bc0a1d82d13bf290a5ebc84e3d17f83a23
divya-lakhtariya <125256150+divya-lakhtariya@users.noreply.github.com>
2023-03-16 11:15:44 +0530

    Add files via upload

---
+++ table.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+  int n,a,c,i;
+  printf("\nEnter the limit:");
+  scanf("%d",&a);
+  printf("\nEnter the table's number:");
+  scanf("%d",&c);
+  while(i<=1)
+  {
+    printf("\n%d*%d=%d",n,c,n*c);
+    n++;
+  }
+
+}
